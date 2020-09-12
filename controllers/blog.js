94b8df6960e3fff21b6d69cc643b66a79330dee9
Debian <bitnami@ip-172-26-4-175.us-east-2.compute.internal>
2020-09-12 01:10:00 +0000

    First Commit

---
+++ controllers/blog.js
@@ -0,0 +1,7 @@
+module.exports.blogadd = function(req, res){
+	res.render('blog-add', {title: 'Blog Add'});
+};
+
+module.exports.bloglist = function(req, res){
+	res.render('blog-list', {title: 'Blog List'});
+};
