9dc76511eaa51074b6b5424e36ca08b93bcb3b95
Debian <bitnami@ip-172-26-4-175.us-east-2.compute.internal>
2020-09-24 21:47:10 +0000

    Added folder for the api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
 require('./app_server/models/db', {useUnifiedTopology: true});
 
 var app = express();
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
+app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
