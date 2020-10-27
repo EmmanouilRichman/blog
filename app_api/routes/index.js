c4bc21afe32df74ef838b650fb7d2d83d60bc24f
Debian <bitnami@ip-172-26-12-186.ec2.internal>
2020-10-27 18:52:20 +0000

    Updated routes to use authentication

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var ctrlBlogs  = require('../controllers/blog');
+var ctrlAuth = require('../controllers/authentication');
 
 /* Setting up routes */
 router.get('/blogs', ctrlBlogs.blogList);
 router.get('/blogs/:blogid', ctrlBlogs.blogReturnOne);
-router.post('/blogs', ctrlBlogs.blogAddOne);
-router.put('/blogs/:blogid', ctrlBlogs.blogUpdate);
-router.delete('/blogs/:blogid', ctrlBlogs.blogDelete);
+router.post('/blogs', auth, ctrlBlogs.blogAddOne);
+router.put('/blogs/:blogid', auth, ctrlBlogs.blogUpdate);
+router.delete('/blogs/:blogid', auth, ctrlBlogs.blogDelete);
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 
 module.exports = router;                            
