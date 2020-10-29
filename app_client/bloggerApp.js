a7d8b76d36d654f67a447678af28060029d62d5f
Debian <bitnami@ip-172-26-12-186.ec2.internal>
2020-10-29 00:15:50 +0000

    Tokens now work for users

--- app_client/bloggerApp.js
+++ app_client/bloggerApp.js
@@ -1,5 +1,70 @@
 var app = angular.module('bloggerApp', ['ngRoute', 'ui.router']);
 
+//*** Authentication Service and Methods **
+app.service('authentication', authentication);
+    authentication.$inject = ['$window', '$http'];
+    function authentication ($window, $http) {
+
+        var saveToken = function (token) {
+            $window.localStorage['blog-token'] = token;
+        };
+
+        var getToken = function () {
+            return $window.localStorage['blog-token'];
+        };
+
+        var register = function(user) {
+            console.log('Registering user ' + user.email + ' ' + user.password);
+            return $http.post('/api/register', user).success(function(data){
+                saveToken(data.data.token);
+          });
+        };
+
+        var login = function(user) {
+           console.log('Attempting to login user ' + user.email + ' ' + user.password);
+            return $http.post('/api/login', user).then(function(data) {
+              saveToken(data.data.token);
+           });
+        };
+
+        var logout = function() {
+            $window.localStorage.removeItem('blog-token');
+        };
+
+        var isLoggedIn = function() {
+          var token = getToken();
+
+          if(token){
+           var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.exp > Date.now() / 1000;
+          } else {
+            return false;
+          }
+        };
+
+        var currentUser = function() {
+          if(isLoggedIn()){
+            var token = getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return {
+              email : payload.email,
+              name : payload.name
+            };
+          }
+        };
+
+        return {
+          saveToken : saveToken,
+          getToken : getToken,
+          register : register,
+          login : login,
+          logout : logout,
+          isLoggedIn : isLoggedIn,
+          currentUser : currentUser
+        };
+}
+
 //configuring routes
 app.config(function ($locationProvider, $routeProvider) {
     $locationProvider.html5Mode({
@@ -157,71 +222,6 @@ app.controller('deleteCtrl', [ '$http', '$routeParams', '$scope','$location', fu
     }
 
 }]);
-
-//*** Authentication Service and Methods **
-app.service('authentication', authentication);
-    authentication.$inject = ['$window', '$http'];
-    function authentication ($window, $http) {
-
-        var saveToken = function (token) {
-            $window.localStorage['blog-token'] = token;
-        };
-
-        var getToken = function () {
-            return $window.localStorage['blog-token'];
-        };
-
-        var register = function(user) {
-            console.log('Registering user ' + user.email + ' ' + user.password);
-            return $http.post('/api/register', user).success(function(data){
-                saveToken(data.token);
-          });
-        };
-
-        var login = function(user) {
-           console.log('Attempting to login user ' + user.email + ' ' + user.password);
-            return $http.post('/api/login', user).then(function(data) {
-              saveToken(data.token);
-           });
-        };
-
-        var logout = function() {
-            $window.localStorage.removeItem('blog-token');
-        };
-
-        var isLoggedIn = function() {
-          var token = getToken();
-
-          if(token){
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-            return payload.exp > Date.now() / 1000;
-          } else {
-            return false;
-          }
-        };
-
-        var currentUser = function() {
-          if(isLoggedIn()){
-            var token = getToken();
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-            return {
-              email : payload.email,
-              name : payload.name
-            };
-          }
-        };
-
-        return {
-          saveToken : saveToken,
-          getToken : getToken,
-          register : register,
-          login : login,
-          logout : logout,
-          isLoggedIn : isLoggedIn,
-          currentUser : currentUser
-        };
-}
 
 app.controller('loginCtrl', [ '$http', '$location', 'authentication', function loginCtrl($htttp, $location, authentication) {
     var vm = this;
